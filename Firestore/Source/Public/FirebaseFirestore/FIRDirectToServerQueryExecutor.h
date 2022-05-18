5be003fdcbcc1460848b48b1f865fba3e6f83af0
Denver Coneybeare <dconeybe@google.com>
2022-05-18 16:33:23 -0400

    FIRDirectToServerQueryExecutor.h: Add factory methods

--- Firestore/Source/Public/FirebaseFirestore/FIRDirectToServerQueryExecutor.h
+++ Firestore/Source/Public/FirebaseFirestore/FIRDirectToServerQueryExecutor.h
@@ -1,16 +1,35 @@
 #import <Foundation/Foundation.h>
 
+@class FIRAggregateQuery;
+@class FIRAggregateQuerySnapshot;
 @class FIRDocumentReference;
+@class FIRDocumentSnapshot;
+@class FIRGroupByQuery;
+@class FIRGroupByQuerySnapshot;
+@class FIRQuery;
+@class FIRQuerySnapshot;
 
 NS_ASSUME_NONNULL_BEGIN
 
 NS_SWIFT_NAME(DirectToServerQueryExecutor)
-@interface FIRDirectToServerQueryExecutor : NSObject
+@interface FIRDirectToServerQueryExecutor<T> : NSObject
 
 /** :nodoc: */
 - (id)init __attribute__((unavailable("FIRDirectToServerQueryExecutor  cannot be created directly.")));
 
-+ (FIRDirectToServerQueryExecutor *)DirectToServerQueryExecutorForDocument:(FIRDocumentReference *)doc;
++ (FIRDirectToServerQueryExecutor<FIRDocumentSnapshot> *)DirectToServerQueryExecutorForDocument:(FIRDocumentReference *)doc;
+
++ (FIRDirectToServerQueryExecutor<FIRQuerySnapshot> *)DirectToServerQueryExecutorForQuery:(FIRQuery *)query;
+
++ (FIRDirectToServerQueryExecutor<FIRAggregateSnapshot> *)DirectToServerQueryExecutorForQuery:(FIRAggregateQuery *)query;
+
++ (FIRDirectToServerQueryExecutor<FIRGroupBySnapshot> *)DirectToServerQueryExecutorForQuery:(FIRGroupByQuery *)query;
+
++ (FIRDirectToServerQueryExecutor<FIRPartitionSnapshot> *)DirectToServerQueryExecutorForQuery:(FIRPartitionQuery *)query;
+
+- (void)getWithCompletion:
+    (void (^)(T *_Nullable snapshot, NSError *_Nullable error))completion
+    NS_SWIFT_NAME(get(completion:));
 
 @end
 
