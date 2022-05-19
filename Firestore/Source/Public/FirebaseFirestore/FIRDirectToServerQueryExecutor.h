880643df7b1355c4c077a9849f3a265ad8d21543
Denver Coneybeare <dconeybe@google.com>
2022-05-18 20:45:51 -0400

    FIRDirectToServerQueryExecutor.h: fix compilation errors

--- Firestore/Source/Public/FirebaseFirestore/FIRDirectToServerQueryExecutor.h
+++ Firestore/Source/Public/FirebaseFirestore/FIRDirectToServerQueryExecutor.h
@@ -6,6 +6,8 @@
 @class FIRDocumentSnapshot;
 @class FIRGroupByQuery;
 @class FIRGroupByQuerySnapshot;
+@class FIRPartitionQuery;
+@class FIRPartitionQuerySnapshot;
 @class FIRQuery;
 @class FIRQuerySnapshot;
 
@@ -17,15 +19,15 @@ NS_SWIFT_NAME(DirectToServerQueryExecutor)
 /** :nodoc: */
 - (id)init __attribute__((unavailable("FIRDirectToServerQueryExecutor  cannot be created directly.")));
 
-+ (FIRDirectToServerQueryExecutor<FIRDocumentSnapshot> *)DirectToServerQueryExecutorForDocument:(FIRDocumentReference *)doc;
++ (FIRDirectToServerQueryExecutor<FIRDocumentSnapshot *> *)DirectToServerQueryExecutorForDocument:(FIRDocumentReference *)doc;
 
-+ (FIRDirectToServerQueryExecutor<FIRQuerySnapshot> *)DirectToServerQueryExecutorForQuery:(FIRQuery *)query;
++ (FIRDirectToServerQueryExecutor<FIRQuerySnapshot *> *)DirectToServerQueryExecutorForQuery:(FIRQuery *)query;
 
-+ (FIRDirectToServerQueryExecutor<FIRAggregateSnapshot> *)DirectToServerQueryExecutorForQuery:(FIRAggregateQuery *)query;
++ (FIRDirectToServerQueryExecutor<FIRAggregateQuerySnapshot *> *)DirectToServerQueryExecutorForAggregateQuery:(FIRAggregateQuery *)query;
 
-+ (FIRDirectToServerQueryExecutor<FIRGroupBySnapshot> *)DirectToServerQueryExecutorForQuery:(FIRGroupByQuery *)query;
++ (FIRDirectToServerQueryExecutor<FIRGroupByQuerySnapshot *> *)DirectToServerQueryExecutorForGroupByQuery:(FIRGroupByQuery *)query;
 
-+ (FIRDirectToServerQueryExecutor<FIRPartitionSnapshot> *)DirectToServerQueryExecutorForQuery:(FIRPartitionQuery *)query;
++ (FIRDirectToServerQueryExecutor<FIRPartitionQuerySnapshot *> *)DirectToServerQueryExecutorForPartitionQuery:(FIRPartitionQuery *)query;
 
 - (void)getWithCompletion:
     (void (^)(T *_Nullable snapshot, NSError *_Nullable error))completion
